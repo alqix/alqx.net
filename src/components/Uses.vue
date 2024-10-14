bc17d851e7d1be47838511a43107ee952a7c76f0
Alp <alpx@tutanota.com>
2024-10-14 23:41:25 +0300

    inital commit

---
+++ src/components/Uses.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="mb-2 font-black text-2xl">uses/</div>
+  <img src="https://skillicons.dev/icons?i=linux,git,vim,neovim,docker,github" class="select-none mb-2" />
+  <img src="https://skillicons.dev/icons?i=c,cs,python,html,css" class="select-none" />
+</template>
